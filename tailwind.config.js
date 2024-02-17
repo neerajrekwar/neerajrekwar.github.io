3d74eb56c690e08c0f0f3260b8e4549508decc0a
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-02-17 06:14:33 +0000

    fa

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./index.html"],
+  content: [
+    './components/**/*.{html,js}',
+    './pages/**/*.{html,js}',
+    './index.html',
+  ],
   theme: {
     extend: {},
   },
