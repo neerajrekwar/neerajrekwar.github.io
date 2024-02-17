50d955f706e895b77eaace65c7ddde17241df118
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-02-17 06:06:49 +0000

    fdg

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}", "./index.html"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
