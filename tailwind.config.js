b4e648d131721b5ca9b68dda89acb74ea3c7924e
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-02-27 22:02:15 +0530

    gs

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,5 +23,17 @@ module.exports = {
     './pages/**/*.{html,js}',
     './index.html',
   ],
-  // ...
+  plugins: [
+    require('taos/plugin')
+  ],
+  safelist: [
+    '!duration-[0ms]',
+    '!delay-[0ms]',
+    'html.js :where([class*="taos:"]:not(.taos-init))'
+  ],
+  content: {
+    relative: true,
+    transform: (content) => content.replace(/taos:/g, ''),
+    files: ['./src/*.{html,js}'],
+  },
 }
