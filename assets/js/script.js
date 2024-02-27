f8c498c3caf148a83cbcaec24293df88f102b1da
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-02-27 23:42:02 +0530

    gd

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,14 @@ const scrollTimeline = new ScrollTimeline({
 });
 
 document.styleSheets[0].insertRule(`@scroll-timeline scrollAnimation { source: ${scrollTimeline}; }`);
+
+window.addEventListener('scroll', function() {
+    updateProgressBar();
+  });
+
+  function updateProgressBar() {
+    var progressBar = document.getElementById('progressBar');
+    var totalHeight = document.body.scrollHeight - window.innerHeight;
+    var progress = (window.scrollY / totalHeight) * 100;
+    progressBar.style.width = progress + '%';
+  }
