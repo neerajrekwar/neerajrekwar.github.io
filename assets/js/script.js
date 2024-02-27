6c7440a77e66f072f195c71e8d76174c8a2e7ab7
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-02-27 23:00:19 +0530

    gs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,8 @@
 // Jvascript
+const scrollTimeline = new ScrollTimeline({
+    scrollSource: document.scrollingElement,
+    orientation: 'block',
+    timeRange: 1,
+});
 
-}
+document.styleSheets[0].insertRule(`@scroll-timeline scrollAnimation { source: ${scrollTimeline}; }`);
