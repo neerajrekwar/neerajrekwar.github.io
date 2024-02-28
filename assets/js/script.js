bb298f66a71c032bee996b72c98193f522e7975c
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-02-29 00:09:00 +0530

    gs

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,3 +17,54 @@ window.addEventListener('scroll', function() {
     var progress = (window.scrollY / totalHeight) * 100;
     progressBar.style.width = progress + '%';
   }
+
+  // scroll for new 2/28/2024 11:42PM
+  const header =document.querySelector("header");
+  const sectionOne = document.querySelector(".home-intro");
+
+  const faders = document.querySelectorAll("fade-in");
+  const sliders = document.querySelectorAll("slide-in");
+
+  const sectionOneOptions = {
+    rootMargin: "-200px 0px 0px 0px"
+  };
+
+  const sectionOneObserver = new IntersectionObserver(function(
+    entries,
+    sectionOneObserver
+   ) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+       header.classList.add("nev-scrolled");
+      } else {
+        header.classList.remove("nav-scrolled")
+      }
+    });
+  },
+  sectionOneOptions);
+
+  sectionOneObserver.observe(sectionOne);
+
+  const appearOptions = {
+   threshold: 0,
+   rootMargin: "opx 0px -100px 0px"
+  };
+
+  const appearOnScroll = new IntersectionObserver(function(
+    entries,
+    appearOnScroll
+  ) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        return;
+      } else {
+        entry.target.classList.add("appear");
+        appearOnScroll.unobserve(entry.target);
+      }
+    });
+  },
+  appearOptions);
+
+  faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+  });
