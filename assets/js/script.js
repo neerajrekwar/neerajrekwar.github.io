9134595552231060a48b568486ff00fea344d518
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-03-18 12:54:50 +0530

    fafda

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,8 +22,8 @@ window.addEventListener('scroll', function() {
   const header = document.querySelector("header");
   const sectionOne = document.querySelector(".home-intro");
 
-  const faders = document.querySelectorAll("fade-in");
-  const sliders = document.querySelectorAll("slide-in");
+  const faders = document.querySelectorAll(".fade-in");
+  const sliders = document.querySelectorAll(".slide-in");
 
   const sectionOneOptions = {
     rootMargin: "-200px 0px 0px 0px"
