32d04be4e0b1702ca027a0fe0797d9ae2fc8d039
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-05-21 18:46:32 +0530

    fdsa

--- next.config.mjs
+++ next.config.mjs
@@ -2,5 +2,11 @@
 const nextConfig = {
     Output: 'export',
 };
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  basePath: isProd ? '/neerajrekwar.github.io' : '',
+  assetPrefix: isProd ? '/neerajrekwar.github.io/' : '',
+};
 
 export default nextConfig;
