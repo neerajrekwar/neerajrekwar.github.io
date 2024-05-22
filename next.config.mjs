1aea929e78e44e591664bdce54c4094c146cc81d
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-05-22 14:55:54 +0530

    Update next.config.mjs
    Signed-off-by: Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,5 @@
 const nextConfig = {
     Output: 'export',
 };
-const isProd = process.env.NODE_ENV === 'production';
-
-module.exports = {
-  basePath: isProd ? '/neerajrekwar.github.io' : '',
-  assetPrefix: isProd ? '/neerajrekwar.github.io/' : '',
-};
 
 export default nextConfig;
