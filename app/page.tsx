60d88301c0e72a5b91fd92ecc134e49fcb90bca8
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-05-26 11:42:10 +0000

    fda

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,28 @@
+import Image from "next/image";
+import profilePic from "../public/me.jpg";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 border-2 border-lime-500 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
+      <section className=" h-screen w-full border-yellow-500">
+        <div className="z-10  border-lime-500 items-center justify-between font-mono text-sm lg:flex-col lg:border-blue-500">
+          <figure className="flex-col flex justify-center">
+            <Image
+              src={profilePic}
+              width={500}
+              height={500}
+              alt="Picture of the author"
+              className="text-center rounded shadow-2"
+            />
 
+            {/* <figcaption>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum quos quidem suscipit a? Ad laudantium accusamus dolorem quae sed est, voluptatibus ipsam harum repudiandae, nam perferendis, at molestiae tempore. Provident!</figcaption> */}
+          </figure>
+          <h2 className=" text-xl sm:text-4xl lg:text-5xl font-bold text-[#242424] ">Neeraj Rekwar</h2>
+          <div className=" left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
+
+          </div>
         </div>
-      </div>
+      </section>
       <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
       </div>
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
