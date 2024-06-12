3ae6047502ec71122625e388b917ace4794ea205
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-06-12 15:52:54 +0530

    gs

--- app/page.tsx
+++ app/page.tsx
@@ -6,29 +6,8 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between p-24">
       <section className=" md:flex border-2 border-lime-500 justify-end place-items-start h-screen w-full text-sm antialiased ">
-        <div className="z-10 dark   flex items-center justify-between font-mono text-sm lg:flex-col lg:border-blue-500">
-          <figure className="flex-col flex justify-center ">
-            <Image
-              src={profilePic}
-              width={500}
-              height={500}
-              alt="Picture of the author"
-              className="text-center rounded shadow-2"
-            />
-            <h2 className=" text-xl sm:text-4xl dark:black lg:text-5xl font-bold text-[#242424] mt-4 pb-2 ">Neeraj Rekwar</h2>
-            <figcaption>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum quos quidem suscipit a? Ad laudantium accusamus dolorem quae sed est, voluptatibus ipsam harum repudiandae, nam perferendis, at molestiae tempore. Provident!</figcaption>
-          </figure>
-        </div>
-        <div className="border-2 left-0 flex h-48  items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <ul>
-            <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos impedit quisquam laborum, ea, eaque nobis velit autem qui quibusdam itaque cupiditate a voluptatem voluptatibus, quia esse possimus facere accusamus adipisci.</li>
-          </ul>
-        </div>
+      
       </section>
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-      </div>
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-      </div>
     </main>
   );
 }
