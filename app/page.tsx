279c611f00952d876d733c0bcf937b167c4993d6
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-06-12 18:16:28 +0530

    dgagfdsfdasgdffdfd

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
+
 import Image from "next/image";
 import profilePic from "../public/me.jpg";
 
 
+
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between p-24">
-      <section className=" md:flex border-2 border-lime-500 justify-end place-items-start h-screen w-full text-sm antialiased ">
-      
-      </section>
+    <main>
+
     </main>
   );
 }
