0b1ab7ca99479f59911a122531c0af1614671a4c
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-06-12 18:49:33 +0530

    gfds

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,19 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main>
-     <section className="h-[50vh] m-2">
-      <div>
-      <Image
-      src="/me.png"
-      width={500}
-      height={500}
-      alt="Picture of the author"
-    />
-      </div>
-      <div></div>
+      <section className="h-[50vh] m-2">
+        <div>
+          <Image
+            src="/me.png"
+            width={500}
+            height={500}
+            alt="Picture of the author"
+          />
+        </div>
+        <div></div>
       </section>
-     <section className="h-[50vh] m-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius neque, atque similique veniam aperiam consequuntur placeat tempore, recusandae iusto quam omnis suscipit? Animi sed possimus repellat quo quae distinctio! Possimus!</section>
-     <section className="h-[50vh] m-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius neque, atque similique veniam aperiam consequuntur placeat tempore, recusandae iusto quam omnis suscipit? Animi sed possimus repellat quo quae distinctio! Possimus!</section>
+      <section className="h-[50vh] m-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius neque, atque similique veniam aperiam consequuntur placeat tempore, recusandae iusto quam omnis suscipit? Animi sed possimus repellat quo quae distinctio! Possimus!</section>
+      <section className="h-[50vh] m-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius neque, atque similique veniam aperiam consequuntur placeat tempore, recusandae iusto quam omnis suscipit? Animi sed possimus repellat quo quae distinctio! Possimus!</section>
     </main>
   );
 }
