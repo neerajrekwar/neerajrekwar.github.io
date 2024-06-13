0bb3bd55074031f2386384d472ea753a1b84abfe
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-06-13 17:16:56 +0530

    fdas

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,7 @@ export default function Home() {
                 width={350}
                 height={350}
                 alt="Picture of the author"
+                className="rounded shadow-[0_2.8px_2.2px_rgba(0,_0,_0,_0.034),_0_6.7px_5.3px_rgba(0,_0,_0,_0.048),_0_12.5px_10px_rgba(0,_0,_0,_0.06),_0_22.3px_17.9px_rgba(0,_0,_0,_0.072),_0_41.8px_33.4px_rgba(0,_0,_0,_0.086),_0_100px_80px_rgba(0,_0,_0,_0.12)]"
               />
             </div>
             <div className="m-2 md:w-[70vw]">
