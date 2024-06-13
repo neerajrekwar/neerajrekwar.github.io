98d88aba23266e6d0fd77c73ed4e1f17d7f454fc
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-06-13 17:51:04 +0530

    fdsa

--- app/page.tsx
+++ app/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
       <section className="m-2 bg-black text-white">
         <div className="p-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam consequatur repellendus saepe omnis amet pariatur sint debitis aut expedita perspiciatis quo nisi optio voluptatem necessitatibus, magni earum reprehenderit ipsum enim.</div>
       </section>
-      <section className="h-[50vh] m-2 bg-white">
+      <section className="h-[50vh] m-2 bg-white dark:bg-black dark:text-white">
         <div className="p-1">
           [![Deploy Next.js site to Pages](https://github.com/neerajrekwar/neerajrekwar.github.io/actions/workflows/nextjs.yml/badge.svg)](https://github.com/neerajrekwar/neerajrekwar.github.io/actions/workflows/nextjs.yml)
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo dolorem exercitationem quam ad reiciendis perspiciatis dignissimos ea assumenda ducimus quasi, non vel quidem, suscipit eius, illo voluptatibus eaque inventore. Veritatis.
