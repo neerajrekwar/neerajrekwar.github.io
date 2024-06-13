a101e3e1d14cee0e33e8c64e2bf48c3bdffedb3b
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-06-13 16:40:36 +0530

    gfs

--- app/page.tsx
+++ app/page.tsx
@@ -13,17 +13,17 @@ export default function Home() {
                 alt="Picture of the author"
                 className="rounded shadow-[0_2.8px_2.2px_rgba(0,_0,_0,_0.034),_0_6.7px_5.3px_rgba(0,_0,_0,_0.048),_0_12.5px_10px_rgba(0,_0,_0,_0.06),_0_22.3px_17.9px_rgba(0,_0,_0,_0.072),_0_41.8px_33.4px_rgba(0,_0,_0,_0.086),_0_100px_80px_rgba(0,_0,_0,_0.12)]"
               />
-            </div>
-            <div className="m-2 md:w-[70vw]">
-              <h2 className="font-bold text-[#212121] text-3xl md:text-4xl lg:text-4xl">Neeraj Rekwar</h2>
-              <figcaption>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius illum architecto hic, dicta perferendis quo tenetur veniam magni quis aliquam, culpa repellat consequatur temporibus voluptatum fugiat voluptatibus doloribus rerum vitae?</figcaption>
-            </div>
-          </figure>
-        </div>
-        <div>
-
-        </div>
-      </section>
+              </div>
+              <div className="m-2 md:w-[70vw]">
+                <h2 className="font-bold text-[#212121] text-3xl md:text-4xl lg:text-4xl">Neeraj Rekwar</h2>
+                <figcaption>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius illum architecto hic, dicta perferendis quo tenetur veniam magni quis aliquam, culpa repellat consequatur temporibus voluptatum fugiat voluptatibus doloribus rerum vitae?</figcaption>
+              </div>
+            </figure>
+          </div>
+          <div>
+  
+          </div>
+        </section>
       <section className="m-2 bg-black text-white">
         <div className="p-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam consequatur repellendus saepe omnis amet pariatur sint debitis aut expedita perspiciatis quo nisi optio voluptatem necessitatibus, magni earum reprehenderit ipsum enim.</div>
       </section>
