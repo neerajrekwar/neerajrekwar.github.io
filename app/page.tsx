9fa5e45aaa90a280f0b6a83640a3ac1dbd3093da
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-06-13 14:28:35 +0530

    gfds

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main>
-      <section className="h-[50vh] m-2">
+      <section className=" m-2">
         <div>
           <Image
             src="/me.jpg"
