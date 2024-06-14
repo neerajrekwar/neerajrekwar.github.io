37b0a831c12adf7275e68ec4f13e1b3db95fec9d
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-06-14 07:00:01 +0000

    gkj

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,17 @@
 import { nextui } from "@nextui-org/theme";
-import type { Config } from "tailwindcss";
 const {
   default: flattenColorPalette,
 } = require("tailwindcss/lib/util/flattenColorPalette");
 
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@nextui-org/theme/dist/components/[object Object].js",
   ],
-  darkMode: "selector",
+  darkMode: "class",
   theme: {
     extend: {
       animation: {
@@ -25,17 +25,13 @@ const config: Config = {
           to: {
             backgroundPosition: "350% 50%, 350% 50%",
           },
-          backgroundImage: {
-            "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-            "gradient-conic":
-              "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-          },
         },
       },
-      plugins: [nextui(), addVariablesForColors],
     },
   },
+  plugins: [nextui(), addVariablesForColors],
 };
+
 // This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
 function addVariablesForColors({ addBase, theme }: any) {
   let allColors = flattenColorPalette(theme("colors"));
@@ -47,4 +43,3 @@ function addVariablesForColors({ addBase, theme }: any) {
     ":root": newVars,
   });
 }
-export default config;
