d77f8e50dab5f221aa752ac368bd8ba63b6f8bd6
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-06-14 08:29:18 +0000

    fda

--- app/components/FlipWords.tsx
+++ app/components/FlipWords.tsx
@@ -8,7 +8,7 @@ export function FlipWordsDemo() {
   return (
     <div className="h-[40rem] flex justify-center items-center px-4">
       <div className="text-4xl mx-auto font-normal text-neutral-600 dark:text-neutral-400">
-        hello, world I'm
+        hello, world I am
         <FlipWords words={words} /> <br />
         websites with Aceternity UI
       </div>
