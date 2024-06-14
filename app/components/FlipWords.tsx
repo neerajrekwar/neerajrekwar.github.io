be4db00e0e93d1eafa827dde3e94aa4ce9460323
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-06-14 15:32:33 +0000

    fdas

--- app/components/FlipWords.tsx
+++ app/components/FlipWords.tsx
@@ -2,17 +2,13 @@ import React from "react";
 import { FlipWords } from "./ui/flip-words";
 
 export function FlipWordsDemo() {
-  const words = ["best", "cute", "beautiful", "modern"];
+  const words = ["better", "cute", "beautiful", "modern"];
 
   return (
     <div className="h-[40rem] flex justify-center items-center px-4">
-      <div className="text-xl  border-2 border-black items-center overflow-hidden sm:text-xl mx-auto font-normal text-neutral-600 dark:text-white">
-        Build, I&apos;m
-        <FlipWords
-          words={words}
-          className="border-2 overflow-hidden border-red-300"
-        />{" "}
-        <br />
+      <div className="text-4xl mx-auto font-normal text-neutral-600 dark:text-neutral-400">
+        Build
+        <FlipWords words={words} /> <br />
         websites with Aceternity UI
       </div>
     </div>
