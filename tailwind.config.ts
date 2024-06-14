3f355700ff793c618a80af3126271c818cb76a4f
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-06-14 08:22:57 +0000

    fdsa

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -25,6 +25,11 @@ module.exports = {
           to: {
             backgroundPosition: "350% 50%, 350% 50%",
           },
+          backgroundImage: {
+            "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+            "gradient-conic":
+              "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+          },
         },
       },
     },
