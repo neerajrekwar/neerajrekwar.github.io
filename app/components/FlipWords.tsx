3687721163e4137dcc03dc4922e9abcbe6294f9a
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-06-14 09:01:24 +0000

    gfdxs

--- app/components/FlipWords.tsx
+++ app/components/FlipWords.tsx
@@ -1,14 +1,13 @@
-"use client";
 import React from "react";
 import { FlipWords } from "./ui/flip-words";
 
 export function FlipWordsDemo() {
-  const words = ["Neeraj Rekwar", "cute", "beautiful", "modern"];
+  const words = ["better", "cute", "beautiful", "modern"];
 
   return (
     <div className="h-[40rem] flex justify-center items-center px-4">
       <div className="text-4xl mx-auto font-normal text-neutral-600 dark:text-neutral-400">
-        hello, world I&apos;am
+        Build, I&apos;m
         <FlipWords words={words} /> <br />
         websites with Aceternity UI
       </div>
