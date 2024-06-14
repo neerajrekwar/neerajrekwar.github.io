0989b1c692ac79a70d3eb20943eb6b1875cfd988
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-06-14 09:16:00 +0000

    fda

--- app/components/FlipWords.tsx
+++ app/components/FlipWords.tsx
@@ -6,9 +6,13 @@ export function FlipWordsDemo() {
 
   return (
     <div className="h-[40rem] flex justify-center items-center px-4">
-      <div className="text-xl sm:text-xl mx-auto font-normal text-neutral-600 dark:text-neutral-400">
+      <div className="text-xl  border-2 border-black items-center overflow-hidden sm:text-xl mx-auto font-normal text-neutral-600 dark:text-white">
         Build, I&apos;m
-        <FlipWords words={words} /> <br />
+        <FlipWords
+          words={words}
+          className="border-2 overflow-hidden border-red-300"
+        />{" "}
+        <br />
         websites with Aceternity UI
       </div>
     </div>
