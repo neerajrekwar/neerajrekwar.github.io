de2af047afe53fddc78bef7a83d49b512c67cc28
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-06-15 10:13:58 +0000

    fgsd

--- app/components/FlipWords.tsx
+++ app/components/FlipWords.tsx
@@ -6,7 +6,7 @@ export function FlipWordsDemo() {
 
   return (
     <div className="h-[40rem] flex justify-center items-center px-4">
-      <div className="md:text-4xl text-sm mx-auto font-normal text-neutral-600 dark:text-white">
+      <div className="sm:text-4xl text-xl mx-auto font-normal text-neutral-600 dark:text-neutral-400">
         Build
         <FlipWords words={words} /> <br />
         websites with Aceternity UI
