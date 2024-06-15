52f14e03f1d44e1c6fce1b042ca49f7204af4843
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-06-15 14:56:27 +0000

    fads

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import HeroSection from "./components/heroSection";
+import { FloatingNavDemo } from "./components/floatingNavBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <FloatingNavDemo />
+        {children}
+      </body>
     </html>
   );
 }
