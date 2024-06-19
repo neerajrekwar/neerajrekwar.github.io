09972d8b100872afe04580f35a4f8879a437d1bc
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-06-19 15:18:28 +0000

    gfgsd

---
+++ app/components/button.tsx
@@ -0,0 +1,20 @@
+"use client";
+import React from "react";
+import { Button } from "./ui/moving-border";
+import Link from "next/link";
+
+export function MovingBorderDemo() {
+  return (
+    <div>
+      
+      <Button
+        borderRadius="1.75rem"
+        className="bg-white dark:bg-slate-900 text-black dark:text-white border-neutral-200 dark:border-slate-800"
+      >
+        <Link href={"/contact"} className="">
+          Reach me
+        </Link>
+      </Button>
+    </div>
+  );
+}
--- app/components/ui/lamp.tsx
+++ app/components/ui/lamp.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { cn } from "@/app/utils/cn";
-
+import Link from "next/link";
 
 export function LampDemo() {
   return (
@@ -15,7 +15,7 @@ export function LampDemo() {
           duration: 0.8,
           ease: "easeInOut",
         }}
-        className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-left text-4xl font-medium tracking-tight text-transparent md:text-7xl"
+        className="mt-8  w-full bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-left text-4xl font-medium tracking-tight text-transparent md:text-7xl"
       >
         hello, world <br />
         I&apos;m Programmer
@@ -25,14 +25,29 @@ export function LampDemo() {
         initial={{ opacity: 0.5, y: 100 }}
         whileInView={{ opacity: 1, y: 0 }}
         transition={{
-          delay: 0.3,
-          duration: 0.8,
+          delay: 0.5,
+          duration: 1,
           ease: "easeInOut",
         }}
-        className="text-xs bg-gradient-to-br from-[#5e716a] to-slate-500  bg-clip-text text-left text-4xl font-medium tracking-tight text-transparent sm:text-sm md:text-2xl pl-4 md:pl-20 md:ml-20"
+        className="text-xs w-full bg-gradient-to-br from-[#5e716a] to-slate-500  bg-clip-text text-left text-4xl font-medium tracking-tight text-transparent"
       >
         solve your bugs in your websites with amazing animations and animations.
       </motion.p>
+      <motion.button
+        initial={{ opacity: 0.5, y: 100 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.7,
+          duration: 1,
+          ease: "easeInOut",
+        }}
+        className="text-xs w-full my-4  text-left text-4xl font-medium tracking-tight text-transparent"
+      ><motion.a>
+          <Link href={"/contact"} className=" border-2 py-1 px-2 rounded-full bg-gradient-to-br from-[#5e716a] to-slate-500 bg-clip-text font-bold">
+            Reach me
+          </Link>
+        </motion.a>
+      </motion.button>
     </LampContainer>
   );
 }
@@ -47,8 +62,8 @@ export const LampContainer = ({
   return (
     <div
       className={cn(
-        "relative flex h-[120vh] flex-col items-center justify-center overflow-hidden bg-slate-950 w-full z-0",
-        className,
+        "relative flex h-[110vh] flex-col items-center justify-center overflow-hidden bg-slate-950 w-full  z-0",
+        className
       )}
     >
       <div className="relative flex w-full flex-1 scale-y-125 items-center justify-center isolate z-0 ">
@@ -111,7 +126,7 @@ export const LampContainer = ({
         <div className="absolute inset-auto z-40 h-44 w-full -translate-y-[12.5rem] bg-slate-950 "></div>
       </div>
 
-      <div className="relative z-50  flex -translate-y-80 flex-col items-center px-5">
+      <div className="relative z-50 text-left flex -translate-y-80 flex-col items-center px-5">
         {children}
       </div>
     </div>
