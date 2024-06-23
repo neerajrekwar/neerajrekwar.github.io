6d4402f90aceddf07c309bcf06eb6834eb8cce04
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-06-23 19:51:28 +0000

    ne

--- app/api/weather/route.tsx
+++ app/api/weather/route.tsx
@@ -1,34 +1,27 @@
-// pages/api/weather-by-ip.ts
-"use client"
-import { NextApiRequest, NextApiResponse } from 'next';
+// app/api/weather/route.ts
+import { NextRequest, NextResponse } from 'next/server';
 import axios from 'axios';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  // Get client IP address from the request
-  const clientIp = req.headers['x-real-ip'] || req.socket.remoteAddress;
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
+  const latitude = searchParams.get('latitude');
+  const longitude = searchParams.get('longitude');
 
-  // Example URL for geolocation API (replace with actual API)
-  const geoApiUrl = `https://ipapi.co/${clientIp}/json/`;
+  const options = {
+    method: 'GET',
+    url: 'https://weatherapi-com.p.rapidapi.com/current.json',
+    params: { q: `${latitude},${longitude}` },
+    headers: {
+      'x-rapidapi-key': 'fb4e3b3dd0mshddc389caebd5192p146dc6jsn1901b7092089',
+      'x-rapidapi-host': 'weatherapi-com.p.rapidapi.com'
+    }
+  };
 
   try {
-    // Fetch geolocation data based on IP
-    const geoResponse = await axios.get(geoApiUrl);
-    const { latitude, longitude } = geoResponse.data;
-
-    // Fetch weather data based on detected coordinates
-    const weatherApiUrl = 'https://weatherapi-com.p.rapidapi.com/current.json';
-    const weatherOptions = {
-      params: { q: `${latitude},${longitude}` },
-      headers: {
-        'x-rapidapi-key': 'fb4e3b3dd0mshddc389caebd5192p146dc6jsn1901b7092089',
-        'x-rapidapi-host': 'weatherapi-com.p.rapidapi.com'
-      }
-    };
-
-    const weatherResponse = await axios.get(weatherApiUrl, weatherOptions);
-    res.status(200).json(weatherResponse.data);
+    const response = await axios.request(options);
+    return NextResponse.json(response.data);
   } catch (error) {
-    console.error('Error fetching weather data:', error);
-    res.status(500).json({ error: 'Failed to fetch weather data' });
+    console.error(error);
+    return NextResponse.error();
   }
 }
