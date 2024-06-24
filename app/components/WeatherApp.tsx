f6e7b3b0b4c4b1e49615855fd1a29d24f362c4e9
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-06-24 05:26:32 +0000

    k

--- app/components/WeatherApp.tsx
+++ app/components/WeatherApp.tsx
@@ -53,7 +53,7 @@ const Weather: React.FC = () => {
       {error ? (
         <p>{error}</p>
       ) : weather ? (
-      <div className='border flex justify-center items-center'>
+      <div className='flex justify-center items-center'>
         <h2 className=''>{weather.location.region}, {weather.location.country}</h2>
         <p className='ml-1 font-bold'>{weather.current.temp_c}°C</p>
         <p className='ml-1'>{weather.current.condition.text}</p>
