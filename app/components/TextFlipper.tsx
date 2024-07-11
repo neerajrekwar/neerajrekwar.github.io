14342cbca4d5d07f1beab381afc8f427293171c3
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-07-11 12:41:30 +0000

    S

--- app/components/TextFlipper.tsx
+++ app/components/TextFlipper.tsx
@@ -27,7 +27,7 @@ const TextFlipper: React.FC = () => {
          initial={{ opacity: 0, y: -20 }}
          animate={{ opacity: 1, y: 0 }}
          exit={{ opacity: 0, y: 10 }}
-         transition={{ duration: 0.8, ease: "easeInOut" }}
+         transition={{ duration: 0.8}}
          className=" z-10 text-3xl sm:text-lg md:text-7xl text-thirdText font-bold"
        >
          {words[index]}
