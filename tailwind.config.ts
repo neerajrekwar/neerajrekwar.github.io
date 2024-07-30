008694112bd90ebbfd17599c168641932cdfa551
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-07-31 02:16:58 +0530

    Update tailwind.config.ts
    Signed-off-by: Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,80 +1,97 @@
-import type { Config } from "tailwindcss"
+import { nextui } from "@nextui-org/theme";
+import plugin from "tailwindcss/plugin";
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
 
-const config = {
-  darkMode: ["class"],
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
-  prefix: "",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./public/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/components/[object Object].js",
+  ],
+  darkMode: ["class", "selector"],
   theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
     extend: {
-      colors: {
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
-        },
-        secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
-        },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
-        },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
-        },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
-        },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
-        },
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
-        },
+      backgroundImage: {
+        "custom-image": "url('/projects/kukai-art-mmkzdAN_jzQ-unsplash.jpg')",
       },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
+      colors: {
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
+        third: "var(--color-third)",
+        four: "var(--color-four)",
+        five: "var(--color-five)",
+        six: "var(--color-six)",
+        seven: "var(--color-seven)",
+        
+      },
+      
+      animation: {
+        aurora: "aurora 60s linear infinite",
+        scroll:
+          "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
       },
       keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
+        scroll: {
+          to: {
+            transform: "translate(calc(-50% - 0.5rem))",
+          },
         },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
+        aurora: {
+          from: {
+            backgroundPosition: "50% 50%, 50% 50%",
+          },
+          to: {
+            backgroundPosition: "350% 50%, 350% 50%",
+          },
         },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
-} satisfies Config
+  plugins: [
+    nextui(),
+    addVariablesForColors,
+    require("@tailwindcss/forms"),
+    plugin(function ({ addBase, theme }) {
+      addBase({
+        ":root": {
+          "--color-primary": theme("colors.primary"),
+          "--color-secondary": theme("colors.secondary"),
+          "--color-third": theme("colors.third"),
+          "--color-four": theme("colors.four"),
+          "--color-five": theme("colors.five"),
+          "--color-six": theme("colors.six"),
+          "--color-seven": theme("colors.seven"),
+          
+        },
+        "@media (prefers-color-scheme: dark)": {
+          ":root": {
+            "--color-primary": theme("colors.primary"),
+            "--color-secondary": theme("colors.secondary"),
+            "--color-third": theme("colors.third"),
+            "--color-four": theme("colors.four"),
+            "--color-five": theme("colors.five"),
+            "--color-six": theme("colors.six"),
+          "--color-seven": theme("colors.seven"),
+          },
+        },
+      });
+    }),
+  ],
+};
 
-export default config
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val]),
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
