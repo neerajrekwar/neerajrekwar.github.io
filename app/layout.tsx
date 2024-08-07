dcc6726fcd4501c9f2b07c0fcbc1c020c8fc4d69
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-08-07 16:42:40 +0000

    gfds

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,27 +24,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="scroll-smooth bg-primary focus:scroll-auto">
-     <head>
-        {/* <GoogleAnalytics
-          gaId='G-0PCHHEHDFJ'
+        <GoogleAnalytics
+          gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID as string}
         />
         <GoogleTagManager
-          gtmId='GTM-MV76385'
-        /> */}
-
-       <Script
-         id="google-tag-manager"
-         strategy="afterInteractive"
-         dangerouslySetInnerHTML={{
-           __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-       new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-       j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-       'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-       })(window,document,'script','dataLayer','GTM-MV3R5HQL');`
-         }}
-         >
-       </Script>
-      </head>
+          gtmId={process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID as string}
+        />
       <body className={dm_Sans.className}>
         <FloatingNavDemo />
         <ThemeProvider>{children}</ThemeProvider>
