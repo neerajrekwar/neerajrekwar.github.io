831cfa5d1e0dfabd6bec5d32a34c5fb656ce4efe
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-08-07 09:06:56 +0000

    gfsd

--- app/components/heroSection.tsx
+++ app/components/heroSection.tsx
@@ -5,6 +5,8 @@ import { motion } from "framer-motion";
 import Link from "next/link";
 import { ShootingStarsAndStarsBackgroundDemo } from "./ShootingVsStarts";
 import AgeCalculator from "./AgeCalculator";
+import CountAnimation from "./CountAnimation";
+import { ImageSlides } from "./ImageSlides";
 
 export function HeroSection() {
   return (
@@ -308,7 +310,7 @@ export function HeroSection() {
                   <motion.li
                     initial={{ opacity: 0, x: 40, y: 40 }}
                     whileInView={{ opacity: 1, x: 0, y: 0 }}
-                    transition={{  delay: 0.6 }}
+                    transition={{ delay: 0.6 }}
                     className="m-2  rounded-lg text-six bg-four"
                   >
                     <svg
@@ -380,9 +382,31 @@ export function HeroSection() {
                     transition={{ delay: 1 }}
                     className="m-2  rounded-lg text-six bg-four"
                   >
-                    
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width={44}
+                      height={44}
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth={2}
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      className="m-2 icon icon-tabler icons-tabler-outline icon-tabler-world-www"
+                    >
+                      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                      <path d="M19.5 7a9 9 0 0 0 -7.5 -4a8.991 8.991 0 0 0 -7.484 4" />
+                      <path d="M11.5 3a16.989 16.989 0 0 0 -1.826 4" />
+                      <path d="M12.5 3a16.989 16.989 0 0 1 1.828 4" />
+                      <path d="M19.5 17a9 9 0 0 1 -7.5 4a8.991 8.991 0 0 1 -7.484 -4" />
+                      <path d="M11.5 21a16.989 16.989 0 0 1 -1.826 -4" />
+                      <path d="M12.5 21a16.989 16.989 0 0 0 1.828 -4" />
+                      <path d="M2 10l1 4l1.5 -4l1.5 4l1 -4" />
+                      <path d="M17 10l1 4l1.5 -4l1.5 4l1 -4" />
+                      <path d="M9.5 10l1 4l1.5 -4l1.5 4l1 -4" />
+                    </svg>
                     <h4 className="px-2 text-xl font-semibold">
-                      Efficient algorithms and data structures
+                      How to Grow Online
                     </h4>
                     <p className="m-2">
                       Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -695,7 +719,7 @@ export function HeroSection() {
                   initial={{ opacity: 0, x: -40, y: -40 }}
                   animate={{ opacity: 1, x: 0, y: 0 }}
                   transition={{ duration: 1.2, delay: 0.4 }}
-                  className="container flex flex-wrap md:grid md:grid-flow-row-dense md:grid-cols-2 md:grid-rows "
+                  className="container flex flex-wrap md:grid md:grid-flow-row-dense md:grid-cols-3 md:grid-rows "
                 >
                   <motion.li
                     initial={{ opacity: 0, x: -40, y: 40 }}
@@ -703,29 +727,21 @@ export function HeroSection() {
                     transition={{ delay: 0.6 }}
                     className="m-2  rounded-lg text-six bg-four"
                   >
-                    <h4 className="px-2 text-4xl font-semibold">
+                    <h4 className="px-2 text-3xl font-semibold">
                       Optimized libraries and frameworks
                     </h4>
                     <div className="m-2">
-                      <motion.div
-                        drag
-                        dragConstraints={{
-                          top: -50,
-                          left: -50,
-                          right: 50,
-                          bottom: 50,
-                        }}
-                      />
+                     
                     </div>
                   </motion.li>
                   <motion.li
                     initial={{ opacity: 0, x: -40, y: 40 }}
                     whileInView={{ opacity: 1, x: 0, y: 0 }}
-                    transition={{ delay: 0.8 }}
-                    className="m-2  rounded-lg text-six bg-four"
+                    transition={{ delay: 0.7 }}
+                    className="m-2 col-span-2 rounded-lg text-six bg-four"
                   >
                    
-                    <h4 className="px-2 text-xl font-semibold">
+                    <h4 className="px-2 text-4xl font-semibold">
                       {" "}
                       Leveraging modern JavaScript features
                     </h4>
@@ -739,34 +755,40 @@ export function HeroSection() {
                   <motion.li
                     initial={{ opacity: 0, x: -40, y: 40 }}
                     whileInView={{ opacity: 1, x: 0, y: 0 }}
+                    transition={{ delay: 0.8 }}
+                    className="m-2 col-span-2 rounded-lg text-six bg-four"
+                  >
+                   
+                    <h4 className="px-2 text-xl font-semibold">
+                      Efficient algorithms and data structures
+                    </h4>
+                    <div className="">
+                      <ImageSlides />
+                    </div>
+                  </motion.li>
+                  <motion.li
+                    initial={{ opacity: 0, x: -40, y: 40 }}
+                    whileInView={{ opacity: 1, x: 0, y: 0 }}
+                    transition={{ delay: 0.9 }}
+                    className="m-2  rounded-lg text-six bg-four"
+                  >
+                   
+                    <h4 className="px-2 text-xl font-semibold">Improved User Experience</h4>
+                    <p className="m-2">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Accusamus error alias unde optio deleniti. Deserunt vero,
+                      ad a.
+                    </p>
+                  </motion.li>
+                  <motion.li
+                    initial={{ opacity: 0, x: -40, y: 40 }}
+                    whileInView={{ opacity: 1, x: 0, y: 0 }}
                     transition={{ delay: 1 }}
-                    className="m-2  rounded-lg text-six bg-four"
+                    className="m-2 rounded-lg text-six bg-four"
                   >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width={44}
-                      height={44}
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeWidth={2}
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      className="m-2 icon icon-tabler icons-tabler-outline icon-tabler-world-www"
-                    >
-                      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                      <path d="M19.5 7a9 9 0 0 0 -7.5 -4a8.991 8.991 0 0 0 -7.484 4" />
-                      <path d="M11.5 3a16.989 16.989 0 0 0 -1.826 4" />
-                      <path d="M12.5 3a16.989 16.989 0 0 1 1.828 4" />
-                      <path d="M19.5 17a9 9 0 0 1 -7.5 4a8.991 8.991 0 0 1 -7.484 -4" />
-                      <path d="M11.5 21a16.989 16.989 0 0 1 -1.826 -4" />
-                      <path d="M12.5 21a16.989 16.989 0 0 0 1.828 -4" />
-                      <path d="M2 10l1 4l1.5 -4l1.5 4l1 -4" />
-                      <path d="M17 10l1 4l1.5 -4l1.5 4l1 -4" />
-                      <path d="M9.5 10l1 4l1.5 -4l1.5 4l1 -4" />
-                    </svg>
+                    
                     <h4 className="px-2 text-xl font-semibold">
-                      How to Grow Online
+                      versions to access the latest performance optimizations
                     </h4>
                     <p className="m-2">
                       Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -777,122 +799,26 @@ export function HeroSection() {
                   <motion.li
                     initial={{ opacity: 0, x: -40, y: 40 }}
                     whileInView={{ opacity: 1, x: 0, y: 0 }}
+                    transition={{ delay: 1.1 }}
+                    className="m-2 col-span-2 rounded-lg text-six bg-four"
+                  >
+                    
+                    <h4 className="px-2 text-xl font-semibold">Balance innovation and stability</h4>
+                    <p className="m-2">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Accusamus error alias unde optio deleniti. Deserunt vero,
+                      ad a.
+                    </p>
+                  </motion.li>
+                  <motion.li
+                    initial={{ opacity: 0, scale: 0.5, x: -40, y: 40 }}
+                    whileInView={{ opacity: 1, scale: 1, x: 0, y: 0 }}
                     transition={{ delay: 1.2 }}
-                    className="m-2  rounded-lg text-six bg-four"
+                    className="m-2 col-span-3 rounded-lg text-six bg-four"
                   >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width={44}
-                      height={44}
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeWidth={2}
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      className="m-2 icon icon-tabler icons-tabler-outline icon-tabler-building-store"
-                    >
-                      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                      <path d="M3 21l18 0" />
-                      <path d="M3 7v1a3 3 0 0 0 6 0v-1m0 1a3 3 0 0 0 6 0v-1m0 1a3 3 0 0 0 6 0v-1h-18l2 -4h14l2 4" />
-                      <path d="M5 21l0 -10.15" />
-                      <path d="M19 21l0 -10.15" />
-                      <path d="M9 21v-4a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v4" />
-                    </svg>
-                    <h4 className="px-2 text-xl font-semibold">Localization</h4>
-                    <p className="m-2">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Accusamus error alias unde optio deleniti. Deserunt vero,
-                      ad a.
-                    </p>
-                  </motion.li>
-                  <motion.li
-                    initial={{ opacity: 0, x: -40, y: 40 }}
-                    whileInView={{ opacity: 1, x: 0, y: 0 }}
-                    transition={{ delay: 1.4 }}
-                    className="m-2 rounded-lg text-six bg-four"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width={44}
-                      height={44}
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeWidth={2}
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      className="m-2 icon icon-tabler icons-tabler-outline icon-tabler-brand-google-analytics"
-                    >
-                      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                      <path d="M10 9m0 1.105a1.105 1.105 0 0 1 1.105 -1.105h1.79a1.105 1.105 0 0 1 1.105 1.105v9.79a1.105 1.105 0 0 1 -1.105 1.105h-1.79a1.105 1.105 0 0 1 -1.105 -1.105z" />
-                      <path d="M17 3m0 1.105a1.105 1.105 0 0 1 1.105 -1.105h1.79a1.105 1.105 0 0 1 1.105 1.105v15.79a1.105 1.105 0 0 1 -1.105 1.105h-1.79a1.105 1.105 0 0 1 -1.105 -1.105z" />
-                      <path d="M5 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
-                    </svg>
+                    
                     <h4 className="px-2 text-xl font-semibold">
-                      Google Analytics
-                    </h4>
-                    <p className="m-2">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Accusamus error alias unde optio deleniti. Deserunt vero,
-                      ad a.
-                    </p>
-                  </motion.li>
-                  <motion.li
-                    initial={{ opacity: 0, x: -40, y: 40 }}
-                    whileInView={{ opacity: 1, x: 0, y: 0 }}
-                    transition={{ delay: 1.6 }}
-                    className="m-2  rounded-lg text-six bg-four"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width={44}
-                      height={44}
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeWidth={2}
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      className="m-2 icon-tabler icons-tabler-outline icon-tabler-mailbox"
-                    >
-                      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                      <path d="M10 21v-6.5a3.5 3.5 0 0 0 -7 0v6.5h18v-6a4 4 0 0 0 -4 -4h-10.5" />
-                      <path d="M12 11v-8h4l2 2l-2 2h-4" />
-                      <path d="M6 15h1" />
-                    </svg>
-                    <h4 className="px-2 text-xl font-semibold">Ads Word</h4>
-                    <p className="m-2">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Accusamus error alias unde optio deleniti. Deserunt vero,
-                      ad a.
-                    </p>
-                  </motion.li>
-                  <motion.li
-                    initial={{ opacity: 0, x: -40, y: 40 }}
-                    whileInView={{ opacity: 1, x: 0, y: 0 }}
-                    transition={{ delay: 1.8 }}
-                    className="m-2 rounded-lg text-six bg-four"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width={44}
-                      height={44}
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeWidth={2}
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      className="m-2 icon-tabler icons-tabler-outline icon-tabler-mailbox"
-                    >
-                      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                      <path d="M10 21v-6.5a3.5 3.5 0 0 0 -7 0v6.5h18v-6a4 4 0 0 0 -4 -4h-10.5" />
-                      <path d="M12 11v-8h4l2 2l-2 2h-4" />
-                      <path d="M6 15h1" />
-                    </svg>
-                    <h4 className="px-2 text-xl font-semibold">
-                      Email Marketing
+                      Rigorously test your application
                     </h4>
                     <p className="m-2">
                       Lorem ipsum dolor sit amet consectetur adipisicing elit.
--- app/demo/page.tsx
+++ app/demo/page.tsx
@@ -1,8 +1,12 @@
 import CountAnimation from "../components/CountAnimation";
+import { ImageSlides } from "../components/ImageSlides";
 
 export default function Demo(){
     return(
         <>
+            <div className="example-container">
+                <ImageSlides />
+              </div>
             <section className="min-h-screen justify-center flex items-center text-white bg-secondary">
                 <CountAnimation />
             </section>
