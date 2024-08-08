ade3d168b0a5846a0de17149c41111e4e42f1e6f
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-08-08 12:23:12 +0000

    gfs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,13 @@
 import { Alata, Source_Code_Pro, DM_Sans } from "next/font/google";
-
+import type { Metadata } from "next";
 import "./globals.css";
 import "./add.css";
 import { FloatingNavDemo } from "./components/floatingNavBar";
 import { ThemeProvider } from "./context/ThemeContext";
 import Footer from "./components/footer";
-import Script from "next/script"
-import  GoogleAnalytics  from './components/GoogleAnalytics';
-import GoogleTagManager from './components/GoogleTagManager';
+import Script from "next/script";
+import GoogleAnalytics from "./components/GoogleAnalytics";
+import GoogleTagManager from "./components/GoogleTagManager";
 
 const dm_Sans = DM_Sans({
   weight: "400",
@@ -19,6 +19,8 @@ const alata = Alata({
   subsets: ["latin"],
 });
 
+export const metadata: Metadata = {};
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -26,18 +28,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="scroll-smooth bg-primary focus:scroll-auto">
-            <GoogleTagManager containerId="GTM-MV3R5HQL"/>
-        
+      <GoogleTagManager containerId="GTM-MV3R5HQL" />
+      <meta
+        name="google-site-verification"
+        content="pevPfl5iK_JHKnEwB5u-hZs9u54tVHi3UJkbJ9k2eWg"
+      />
       <body className={dm_Sans.className}>
         <FloatingNavDemo />
-        <GoogleAnalytics
-          gaId='G-0PCHHEHDFJ' 
-        />
+        <GoogleAnalytics gaId="G-0PCHHEHDFJ" />
         <ThemeProvider>{children}</ThemeProvider>
 
         <Footer />
       </body>
-      
     </html>
   );
 }
