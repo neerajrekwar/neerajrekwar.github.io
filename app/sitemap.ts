a46866f12882e5c9ea1d7cdf2160920812835995
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-08-08 12:47:13 +0000

    gfds

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,54 +1,30 @@
-import { MetadataRoute } from "next";
+import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://neerajrekwar.gihub.io/",
+      url: 'https://neerajrekwar.gihub.io',
       lastModified: new Date(),
-      changeFrequency: "yearly",
-      alternates: {
-        languages: {
-          es: "https://neerajrekwar.gihub.io/es",
-          de: "https://neerajrekwar.gihub.io/de",
-        },
-      },
+      changeFrequency: 'yearly',
       priority: 1,
     },
     {
-      url: "https://neerajrekwar.gihub.io/about",
+      url: 'https://neerajrekwar.gihub.io/about',
       lastModified: new Date(),
-      changeFrequency: "monthly",
-      alternates: {
-        languages: {
-          es: "https://neerajrekwar.gihub.io/es/about",
-          de: "https://neerajrekwar.gihub.io/de/about",
-        },
-      },
+      changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: "https://neerajrekwar.gihub.io/blog",
+      url: 'https://neerajrekwar.gihub.io/blog',
       lastModified: new Date(),
-      changeFrequency: "monthly",
-      alternates: {
-        languages: {
-          es: "https://neerajrekwar.gihub.io/es/blog",
-          de: "https://neerajrekwar.gihub.io/de/blog",
-        },
-      },
-      priority: 0.8,
+      changeFrequency: 'weekly',
+      priority: 0.5,
     },
     {
-      url: "https://neerajrekwar.gihub.io/contact",
+      url: 'https://neerajrekwar.gihub.io/contact',
       lastModified: new Date(),
-      changeFrequency: "monthly",
-      alternates: {
-        languages: {
-          es: "https://neerajrekwar.gihub.io/es/contact",
-          de: "https://neerajrekwar.gihub.io/de/contact",
-        },
-      },
-      priority: 0.8,
+      changeFrequency: 'weekly',
+      priority: 0.5,
     },
-  ];
+  ]
 }
