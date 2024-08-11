b2a95a8c4c1de51d931f7f6595f8f4c7f284d0d8
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-08-11 14:19:14 +0000

    gfsd

--- next.config.mjs
+++ next.config.mjs
@@ -5,10 +5,7 @@ const nextConfig = {
   images: {
     domains: ['images.unsplash.com'],
   },
-  webpack: (config) => {
-    config.cache = false;
-    return config;
-  },
+  
 };
 
 export default nextConfig;
