2bc968320eb8be9466980d22f95238c748f4fb1d
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-08-22 20:02:45 +0000

    fda

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,8 +4,6 @@ import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { ToastAction } from "@/components/ui/toast";
 import { useToast } from "@/components/ui/use-toast";
-import { BackgroundGradientAnimation } from "../components/ui/background-gradient-animation";
-
 
 const ContactPage: React.FC = () => {
   const { toast } = useToast();
@@ -61,92 +59,97 @@ const ContactPage: React.FC = () => {
   };
 
   return (
-    <BackgroundGradientAnimation>
-     <div className="max-w-6xl min-h-screen mx-auto items-center justify-center lg:flex">
-       <div className="basis-1/2 border aspect-square">
-         <h1 className="text-3xl text-four font-bold mb-6">
-         Facilis blanditiis impedit atque.</h1>
+    <main className="p-8 bgrid2 bg-primary min-h-screen ">
+     <section className="max-w-6xl  mx-auto m-auto rounded  sm:flex sm:border-rose-600 md:border-blue-400 lg:w-[70%] lg:flex-1 lg:border-yellow-500">
+       <h6 className="mb-2 p-2 text-center text-3xl font-bold text-[#353535]">Reach at me</h6>
+       <p className="m-2 text-base">Numquam ab temporibus aut nulla doloremque suscipit, ex facere.</p>
+       <div>
+         <div className="basis-1/2 p-2 aspect-square bg-six rounded-xl m-2">
+            <h1 className="text-3xl text-four font-bold mb-6">
+            Facilis blanditiis impedit atque.</h1>
+            <p className="m-3 text-xl">let&#39;s talk about something amazing!</p>
+          </div>
+
+          <div className="basis-1/2 aspect-square rounded-xl m-2  bg-secondary p-2 font-semibold  sm:w-[60%]">
+            <form
+              action="https://formspree.io/f/mnqebqjq"
+              method="POST"
+              onSubmit={handleSubmit}
+              className="space-y-4  bg-custom-radial m-auto max-w-4xl"
+            >
+              <div className="p-2">
+                <label htmlFor="name" className="block text-sm font-semibold  text-six">
+                  Name
+                </label>
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  value={formData.name}
+                  onChange={handleChange}
+                  required
+                  className="mt-1 border-input bg-[#ffffff0a]  ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring focus-visible:ring-2 flex h-10 w-full rounded border-none bg-primary px-3 py-2 text-sm  file:border-0 file:bg-transparent file:text-sm file:font-medium focus:bg-seven disabled:cursor-not-allowed disabled:opacity-50 focus:ring-four  focus:border-four"
+                />
+              </div>
+
+              <div className="p-2">
+                <label htmlFor="email"  className="block text-sm font-semibold  text-six">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  required
+                  className="mt-1 border-input bg-[#ffffff0a] ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring focus-visible:ring-2 flex h-10 w-full rounded border-none bg-primary px-3 py-2 text-sm  file:border-0 file:bg-transparent file:text-sm file:font-medium focus:bg-seven disabled:cursor-not-allowed disabled:opacity-50 focus:ring-four  focus:border-four"
+                />
+              </div>
+
+              <div className="p-2">
+                <label htmlFor="subject"  className="block text-sm font-semibold  text-six">
+                  Subject
+                </label>
+                <input
+                  type="text"
+                  id="subject"
+                  name="subject"
+                  value={formData.subject}
+                  onChange={handleChange}
+                  required
+                  className="mt-1 border-input bg-[#ffffff0a] ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring focus-visible:ring-2 flex h-10 w-full rounded border-none bg-primary px-3 py-2 text-sm  file:border-0 file:bg-transparent file:text-sm file:font-medium focus:bg-seven disabled:cursor-not-allowed disabled:opacity-50 focus:ring-four  focus:border-four"
+                />
+              </div>
+
+              <div className="p-2">
+                <label htmlFor="message"  className="block text-sm font-semibold  text-six">
+                  Message
+                </label>
+                <textarea
+                  id="message"
+                  name="message"
+                  value={formData.message}
+                  onChange={handleChange}
+                  required
+                  rows={4}
+                  className="mt-1 border-input bg-[#ffffff0a] ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring-2 focus-visible:ring-2 flex h-22 w-full rounded border-none bg-primary px-3 py-2 text-sm  file:border-0 file:bg-transparent file:text-sm file:font-medium focus:bg-seven disabled:cursor-not-allowed disabled:opacity-50 focus:ring-four focus:border-four"
+                />
+              </div>
+
+              <div className="p-2">
+                <Button
+                  type="submit"
+                  className="w-fit inline-flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-four  hover:opacity-75 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  Send Message
+                </Button>
+              </div>
+            </form>
+          </div>
        </div>
-
-       <div className="basis-1/2 aspect-square border ">
-         <form
-           action="https://formspree.io/f/mnqebqjq"
-           method="POST"
-           onSubmit={handleSubmit}
-           className="space-y-4 w- m-auto max-w-4xl"
-         >
-           <div>
-             <label htmlFor="name" className="block text-sm font-medium text-secondary">
-               Name
-             </label>
-             <input
-               type="text"
-               id="name"
-               name="name"
-               value={formData.name}
-               onChange={handleChange}
-               required
-               className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-five focus:border-four sm:text-sm"
-             />
-           </div>
-
-           <div>
-             <label htmlFor="email"  className="block text-sm font-medium text-secondary">
-               Email
-             </label>
-             <input
-               type="email"
-               id="email"
-               name="email"
-               value={formData.email}
-               onChange={handleChange}
-               required
-               className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-five focus:border-four sm:text-sm"
-             />
-           </div>
-
-           <div>
-             <label htmlFor="subject"  className="block text-sm font-medium text-secondary">
-               Subject
-             </label>
-             <input
-               type="text"
-               id="subject"
-               name="subject"
-               value={formData.subject}
-               onChange={handleChange}
-               required
-               className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-five focus:border-four sm:text-sm"
-             />
-           </div>
-
-           <div>
-             <label htmlFor="message"  className="block text-sm font-medium text-secondary">
-               Message
-             </label>
-             <textarea
-               id="message"
-               name="message"
-               value={formData.message}
-               onChange={handleChange}
-               required
-               rows={4}
-               className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-five focus:border-four sm:text-sm"
-             />
-           </div>
-
-           <div>
-             <Button
-               type="submit"
-               className="w-fit inline-flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-four  hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-             >
-               Send Message
-             </Button>
-           </div>
-         </form>
-       </div>
-     </div>
-    </BackgroundGradientAnimation>
+     </section>
+    </main>
   );
 };
 
