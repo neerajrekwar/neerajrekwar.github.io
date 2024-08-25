b9e1999af500e3d1e8148c75eebfcf80c36a5fe2
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-08-25 20:00:51 +0000

    gfds

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,14 @@
 /** @type {import('next').NextConfig} */
 
-
 const nextConfig = {
-  output: 'export',
+  output: 'export',  // Ensures static export
   images: {
-    domains: ['images.unsplash.com'],
+    domains: ['images.unsplash.com'],  // Allow images from specific domains
   },
   webpack: (config) => {
-    config.cache = false;
+    config.cache = false;  // Disable cache for webpack builds
     return config;
   },
-   
 };
 
 export default nextConfig;
