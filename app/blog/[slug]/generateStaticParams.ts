e94260163cf54e8a6ad106115adcd61487c01897
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-08-26 08:03:06 +0000

    fda

--- app/blog/[slug]/generateStaticParams.ts
+++ app/blog/[slug]/generateStaticParams.ts
@@ -3,5 +3,12 @@ import { posts } from '../data/posts';
 export async function generateStaticParams() {
   return posts.map((post) => ({
     slug: post.slug,  // Generate paths based on the `slug`
-  }));
+  })); 
+}
+export default function Page({ params }) {
+  const { slug } = params
+
+  return (
+ <div>{slug}</div>
+)
 }
