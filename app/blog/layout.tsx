3a01f4b64b6a19e3db2402e3162b564f97637cde
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-08-29 14:06:18 +0000

    gfs

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -11,18 +11,20 @@ export default function RootLayout({
       <body className="bg-primary min-h-screen">
         <header className="h-44  bg-custom-radial-md md:flex justify-between text-five ">
           <div className="basis-1/3  sm:h-auto p-4 ">
-            <h1 className="text-3xl">Blog about something</h1>
+            <h1 className="text-3xl text-">Blog about something</h1>
 
             <p>problems are create by itself it's not heppen </p>
           </div>
           <div className="basis-1/3 mx-2 sm:h-auto p-4">
-            <ul className="flex justify-center items-center font-semibold flex-row gap-2">
-              <li className="border-four border-2 font-semibold flex justify-center  items-center text-four p-1 rounded-full px-3">
+            <ul className="text-four flex justify-center items-center font-semibold flex-row gap-2">
+              <li className="border-four border-2 font-semibold flex justify-center  items-center  p-1 rounded-full px-3">
                 suggest a ideas
               </li>
-              <li>
+              <li className="text-sm md:text-base ">
                 {new Date().toLocaleDateString("en-US", {
                   day: "numeric",
+      dayPeriod: 'narrow',
+      weekday: 'short',
                 })}
               </li>
             </ul>
