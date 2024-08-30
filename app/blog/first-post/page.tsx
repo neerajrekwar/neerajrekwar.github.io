75fd2ef9a9108a9d142efb6588b1c3f05f988c05
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-08-30 15:30:33 +0530

    fda

--- app/blog/first-post/page.tsx
+++ app/blog/first-post/page.tsx
@@ -12,6 +12,7 @@ export default function FirstPostPage() {
       
       </div>
       <article className="prose max-w-none p-4">
+        <span>{post?.date}</span>
         <h1 className="text-3xl md:text-4xl font-bold mb-4">{post?.title}</h1>
         <p className='first-letter:text-2xl first-letter:font-bold first-letter:ml-1 '>
           This is an introductory paragraph. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
