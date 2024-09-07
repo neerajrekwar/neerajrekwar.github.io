fd94e441af971965cadca6359683e95ad1512f16
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-09-07 17:22:51 +0530

    Update BackgroundAurra.tsx
    Signed-off-by: Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>

--- app/components/BackgroundAurra.tsx
+++ app/components/BackgroundAurra.tsx
@@ -27,7 +27,7 @@ export function AuroraBackgroundDemo() {
               <motion.div
                 whileDrag={{}}
                 initial={{ rotate: 160, scale: 0.2, x: -200, x2: 0, y: -200 }}
-                animate={{ rotate: 360, scale: 0.1, x: 400, x2: 0, y: 80 }}
+                animate={{ rotate: 360, scale: 0.1, x: 400, x2: 0, y: 200 }}
                 whileInView={{ rotate: 0, scale: 1, x2: -1040, x: 0, y: 0 }}
                 transition={{
                   type: "spring",
