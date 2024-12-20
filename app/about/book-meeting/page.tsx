76777b8e5a1a28cbbd507e5cb001f150c1a2fdb8
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-12-20 18:11:24 +0530

    Update page.tsx
    Signed-off-by: Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>

--- app/about/book-meeting/page.tsx
+++ app/about/book-meeting/page.tsx
@@ -1,4 +1,26 @@
-<!-- Calendly inline widget begin -->
-<div class="calendly-inline-widget" data-url="https://calendly.com/neerajrekwar817/new-meeting" style="min-width:320px;height:700px;"></div>
-<script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script>
-<!-- Calendly inline widget end -->
+import Image from "next/image"
+import Link from 'next/link';
+import { useEffect } from "react";
+
+const CalendlyWidget = () => {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://assets.calendly.com/assets/external/widget.js";
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return (
+    <div
+      className="calendly-inline-widget"
+      data-url="https://calendly.com/neerajrekwar817/new-meeting"
+      style={{ minWidth: "320px", height: "700px" }}
+    ></div>
+  );
+};
+
+export default CalendlyWidget;
