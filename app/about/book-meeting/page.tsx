6560302c83c9a7f2ff454e1c271f99ca83735d5f
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-12-20 14:28:06 +0000

    fda

--- app/about/book-meeting/page.tsx
+++ app/about/book-meeting/page.tsx
@@ -1,31 +1,22 @@
-"use client";
+
+import CalendlyWidget from "@/components/Book";
 import { Metadata } from "next";
-import { useEffect } from "react";
+
 export const metadata: Metadata = {
   title: "Book meeting",
   description:
     "Let's make together.",
 };
 
-const CalendlyWidget = () => {
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://assets.calendly.com/assets/external/widget.js";
-    script.async = true;
-    document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
-
+const book = () => {
+ 
+  
   return (
     <div
-      className="calendly-inline-widget"
-      data-url="https://calendly.com/neerajrekwar817/new-meeting"
-      style={{ minWidth: "320px", height: "700px" }}
-    ></div>
+      
+    ><CalendlyWidget/></div>
   );
 };
 
-export default CalendlyWidget;
+
+export default book
---
+++ components/Book.tsx
@@ -0,0 +1,25 @@
+
+'use client'
+import { useEffect } from "react";
+const CalendlyWidget = () => {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://assets.calendly.com/assets/external/widget.js";
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return (
+    <div
+      className="calendly-inline-widget"
+      data-url="https://calendly.com/neerajrekwar817/new-meeting"
+      style={{ minWidth: "320px", height: "700px" }}
+    ></div>
+  );
+};
+
+export default CalendlyWidget;
