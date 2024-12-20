d2c2f62d5b110697ca04459546ebd92eb42072cc
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-12-20 18:07:28 +0530

    Create page.tsx
    Signed-off-by: Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>

---
+++ app/about/book-meeting/page.tsx
@@ -0,0 +1,4 @@
+<!-- Calendly inline widget begin -->
+<div class="calendly-inline-widget" data-url="https://calendly.com/neerajrekwar817/new-meeting" style="min-width:320px;height:700px;"></div>
+<script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script>
+<!-- Calendly inline widget end -->
