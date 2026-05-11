830d77ac4eda01f2b0562ea50fab319e13bf0744
neerajrekwar817 <neerajrekwar817@gmail.com>
2026-05-11 08:00:14 +0000

    hj

--- components/PixelTransition.tsx
+++ components/PixelTransition.tsx
@@ -33,7 +33,6 @@ const PixelTransition: React.FC<PixelTransitionProps> = ({
   const [isActive, setIsActive] = useState<boolean>(false);
 
   const isTouchDevice =
-    'ontouchstart' in window || navigator.maxTouchPoints > 0 || window.matchMedia('(pointer: coarse)').matches;
     typeof window !== 'undefined'
       ? 'ontouchstart' in window || navigator.maxTouchPoints > 0 || window.matchMedia('(pointer: coarse)').matches
       : false;
